3d3bb1c9c44034233f5624b8bf3fe985db58dbd8
Hernando Carbonell <nandoe07@gmail.com>
2020-10-09 09:56:28 -0500

    Terminado SeeWhereVideo

--- src/components/seeWhereVideo/SeeWhereVideo.js
+++ src/components/seeWhereVideo/SeeWhereVideo.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import playVideo from './../../assets/Group_4.svg'
+import closeVideo from './../../assets/close_icon.png'
 
 export const SeeWhereVideo = () => {
+
+    const [state, setState] = useState(false)
+
+    const handleClickToggleVideo = () => {
+        setState(!state)
+    }
+
     return (
         <section className="seewherevideo">
             <h2>See where you stand</h2>
-            <p class="p2 container2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do Lorem ipsum dolor sit amet, consectetur adipiscing</p>
-
+            <p className="p2 container2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+            <div className="videosw">
+                <img
+                    src={playVideo}
+                    alt="Video Image"
+                    onClick={handleClickToggleVideo}
+                />
+            </div>
+            {
+                (state)
+                &&
+                (<div className="videout" onClick={handleClickToggleVideo}>
+                    <div>
+                        <div className="contdivvideo">
+                            <img src={closeVideo} alt="X" />
+                            <iframe id="videoiframe" src="https://player.vimeo.com/video/95746815" width="1069" height="601" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>)
+            }
         </section>
     )
 }
